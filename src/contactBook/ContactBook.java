ce1c90496fdf3d708666f5c93c8f92977ff4d3bf
Diogo Gomes <dajgomes@campus.fct.unl.pt>
2026-09-17 17:56:23 +0100

    Fiz o EP

--- src/contactBook/ContactBook.java
+++ src/contactBook/ContactBook.java
@@ -88,6 +88,16 @@ public class ContactBook {
         return (currentContact >= 0 ) && (currentContact < counter);
     }
 
+    public boolean EP(){
+        for(int i =0;i<contacts.length-1;i++){
+            for (int j = i+1; j<contacts.length;j++){
+                if (contacts[i].getPhone()==contacts[j].getPhone())
+                    return true;
+            }
+        }
+        return false;
+    }
+
     //Pre: hasNext()
     public Contact next() {
         return contacts[currentContact++];
